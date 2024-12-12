db35aecab691adbf17da45218e6030e490c1867b
Diogo Jorge <diogofernandoj@gmail.com>
2024-12-12 14:36:11 -0300

    feat: add FavoritesPage

---
+++ app/favorites/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { columns } from "../_components/columns";
+import { DataTable } from "../_components/data-table";
+import { useRadiosContext } from "../_hooks/use-radios-context";
+
+export default function FavoritesPage() {
+  const { radios, hasNextPage, fetchNextPage } = useRadiosContext();
+
+  const favorites = radios.filter((radio) => radio.favorite);
+
+  return (
+    <div className="h-full w-full space-y-8 rounded-lg p-8">
+      <div className="flex w-full flex-col lg:flex-row lg:items-center lg:justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-gray-500">
+            Radio Browser
+          </span>
+          <h2 className="text-xl font-semibold text-black">Favorites</h2>
+        </div>
+      </div>
+      <DataTable
+        columns={columns}
+        data={favorites}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+      />
+    </div>
+  );
+}
